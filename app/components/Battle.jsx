8bb006f807e118cc906786b1d68a09509112a1b8
Morgan Senkal <morgan.senkal@metaltoad.com>
2019-03-03 17:38:56 -0800

    Add PlayerInput component

--- app/components/Battle.jsx
+++ app/components/Battle.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var Link = require('react-router-dom').Link;
+var PropTypes = require('prop-types');
 
 // Parent component for 'Battle' page
 class Battle extends React.Component {
@@ -18,21 +18,101 @@ class Battle extends React.Component {
   }
 
 
-  handleUpdatePlayer () {
+  handleUpdatePlayer (id, username) {
     this.setState(function () {
-      
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=150';
+
+      return newState;
     });
   }
 
   render () {
     return (
-      <div>Battle</div>
+      <div>
+        <div className='row'>
+          {!this.state.playerOneName &&
+            <PlayerInput 
+              playerID='playerOne'
+              label='Player One'
+              onSubmit={this.handleUpdatePlayer}
+            />
+          }
+          {!this.state.playerTwoName &&
+            <PlayerInput 
+              playerID='playerTwo' 
+              label='Player Two'
+              onSubmit={this.handleUpdatePlayer}
+            />
+          }
+        </div>
+      </div>
     );    
   }
 }
 
-function PlayerInput (props) {
 
+class PlayerInput extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange (event) {
+    var updatedValue = event.target.value;
+
+    this.setState(function () {
+      return {
+        username: updatedValue,
+      };
+    });
+  }
+
+  handleSubmit (event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.props.playerID, this.state.username);
+  }
+
+  render () {
+    return (
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label 
+          className='battle-player-header' 
+          htmlFor='username'>
+          {this.props.label}
+        </label>
+        <input 
+          id='username' 
+          placeholder='github username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange} />
+        <button
+          className='button'
+          type='submit'
+          disabled={!this.state.username}>
+          {'Submit'}
+        </button>
+      </form>
+    );
+  }
 }
+
+PlayerInput.propTypes = {
+  playerID: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 
 module.exports = Battle;
